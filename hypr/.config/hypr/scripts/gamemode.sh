1bc1ca290ebd113f43a1809ed3d2e91bbb32aef0
Raider_Dude <raider_dude100@hotmail.com>
2025-11-30 16:35:38 +0300

    new wallpapers

--- hypr/.config/hypr/scripts/gamemode.sh
+++ hypr/.config/hypr/scripts/gamemode.sh
@@ -1,13 +1,8 @@
 !/usr/bin/env sh
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ] ; then
-    exec echo -e "{\n    "\"include\"": [\n    "\"~/.config/waybar/themes/minimal\""\n    ],\n}" > ~/.config/waybar/config &
-    exec echo "@import 'themes/minimal.css';" > ~/.config/waybar/style.css &
-    exec ~/.config/hypr/scripts/reload.sh &
-    exec pkill swaync &
+    exec pkill waybar &
     exec pkill hypridle &
-    exec echo "@theme \"~/.config/rofi/themes/minimal.rasi\"" > ~/.config/rofi/config.rasi &
-    notify-send -e "Gamemode Activated"
     hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:shadow:enabled 0;\
@@ -17,12 +12,10 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword general:border_size 0;\
         keyword decoration:rounding 0"
     exit
+    notify-send -e "Gamemode Activated"
 fi
-    exec echo "@import 'themes/default.css';" > ~/.config/waybar/style.css &
-    exec echo -e "{\n    "\"include\"": [\n    "\"~/.config/waybar/themes/default\""\n    ],\n}" > ~/.config/waybar/config &
-    exec ~/.config/hypr/scripts/reload.sh &
     exec hypridle &
-    exec echo "@theme \"~/.config/rofi/themes/rounded.rasi\"" > ~/.config/rofi/config.rasi &
+    exec waybar &
     notify-send -e "Gamemode Deactivated"
     hyprctl reload
 exit
