4a404cdb61d58cbb86a35e14284c35e0ec002376
RaiderDude100 <raider_dude100@hotmail.com>
2026-01-11 22:37:28 +0300

    Changed neovim config and added plugins

--- hypr/.config/hypr/scripts/search.sh
+++ hypr/.config/hypr/scripts/search.sh
@@ -1,21 +1,11 @@
-#!/bin/bash
-
-# Description: Fuzzy find files using fzf with bat preview, open in vim
-
-# Check if fzf, bat, and vim are installed
-for cmd in fzf bat vim; do
-    if ! command -v "$cmd" &> /dev/null; then
-        echo "Error: '$cmd' is not installed. Please install it and try again."
-        exit 1
-    fi
-done
+#!/bin/sh
 
 # Run fzf with bat preview
-FILE=$(fzf --algo=v1 -e --preview 'bat --style=numbers --color=always --line-range :500 {}' --height=80% --border)
+FILE=$(fzf --algo=v1 -e --preview 'bat --style=numbers --color=always --line-range :500 {}')
 
 # If a file was selected, open it in vim
 if [[ -n "$FILE" ]]; then
-    vim "$FILE"
+    nvim "$FILE"
 else
     echo "No file selected."
 fi
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,6 +5,8 @@ vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.clipboard = "unnamedplus"
 
 vim.cmd [[
   highlight clear
@@ -29,8 +31,8 @@ vim.cmd [[
 
 -- Floating windows transparency
 vim.cmd [[
-  highlight clear NormalFloat
-  highlight clear FloatBorder
+  highlight NormalFloat ctermbg=0
+  highlight FloatBorder ctermbg=0
 ]]
 
 -- Highlight
@@ -47,3 +49,162 @@ vim.cmd [[
   highlight CursorLine cterm=NONE ctermbg=0 guibg=NONE
   highlight CursorLineNr cterm=bold ctermfg=3 guifg=NONE
 ]]
+
+vim.g.mapleader = " "
+
+-------------------------------------------------
+-- BOOTSTRAP lazy.nvim (PLUGIN MANAGER)
+-------------------------------------------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-------------------------------------------------
+-- PLUGINS
+-------------------------------------------------
+require("lazy").setup({
+
+  -------------------------------------------------
+  -- FILE EXPLORER
+  -------------------------------------------------
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({
+        respect_buf_cwd = true,
+        sync_root_with_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+        },
+      })
+
+      vim.keymap.set(
+        "n",
+        "<leader>e",
+        ":NvimTreeToggle<CR>",
+        { silent = true }
+      )
+    end,
+  },
+
+
+  -------------------------------------------------
+  -- TREESITTER (SYNTAX HIGHLIGHTING)
+  -------------------------------------------------
+  {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "python" },
+    highlight = {
+      enable = true,
+      },
+    },
+  },
+
+
+  -------------------------------------------------
+  -- LSP (PYTHON INTELLIGENCE)
+  -------------------------------------------------
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+    vim.lsp.config("pyright", {
+      settings = {
+        python = {
+          analysis = {
+            autoSearchPaths = true,
+            useLibraryCodeForTypes = true,
+                    },
+                },
+            },
+        })
+
+     vim.lsp.enable("pyright")
+    end,
+  },
+
+
+  -------------------------------------------------
+  -- AUTOCOMPLETION
+  -------------------------------------------------
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+        },
+      })
+    end
+  },
+
+  -------------------------------------------------
+  -- DEBUGGER (OPTIONAL, IDLE-LIKE)
+  -------------------------------------------------
+  { "mfussenegger/nvim-dap" },
+  {
+    "mfussenegger/nvim-dap-python",
+    config = function()
+      require("dap-python").setup("python3")
+    end
+  },
+
+  -------------------------------------------------
+  -- STATUS LINE (OPTIONAL BUT NICE)
+  -------------------------------------------------
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup()
+    end
+  },
+})
+
+-------------------------------------------------
+-- KEYMAPS (RUN PYTHON LIKE IDLE)
+-------------------------------------------------
+vim.keymap.set("n", "<F5>", function()
+  vim.cmd("w")
+  vim.cmd("split | terminal python3 %")
+end)
+
+-------------------------------------------------
+-- LSP KEYMAPS (IDE FEATURES)
+-------------------------------------------------
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local opts = { buffer = args.buf }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  end
+})
