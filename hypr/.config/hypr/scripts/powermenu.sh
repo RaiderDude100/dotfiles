6b397d4754678af1714a063c248c638df6582121
RaiderDude100 <raider_dude100@hotmail.com>
2026-01-11 11:21:06 +0300

    Changed a few stuff and added suspend option

--- hypr/.config/hypr/scripts/powermenu.sh
+++ hypr/.config/hypr/scripts/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MENU="Shutdown|Restart|Logout|Lock"
+MENU="Shutdown|Suspend|Restart|Logout|Lock"
 DIALOG_RESULT=$(echo $MENU | rofi -sep "|" -dmenu -i -p "System Configuration" -hide-scrollbar -tokenize -lines 6 -width 50 -padding 50 -disable-history)
 
 echo "This result is : $DIALOG_RESULT"
@@ -10,13 +10,17 @@ if [ "$DIALOG_RESULT" = "Shutdown" ];
 then
 	exec systemctl poweroff
 
+elif [ "$DIALOG_RESULT" = "Suspend" ];
+then
+	exec systemctl suspend
+    
 elif [ "$DIALOG_RESULT" = "Restart" ];
 then
 	exec systemctl reboot
 
 elif [ "$DIALOG_RESULT" = "Logout" ];
 then
-	exec hyprctl dispatch exit
+	exec loginctl terminate-user $USER
 
 elif [ "$DIALOG_RESULT" = "Lock" ];
 then
