ef81865d5aa38a436b1ae4b01f35c61f0bd2452d
RaiderDude100 <raider_dude100@hotmail.com>
2026-01-12 21:52:50 +0300

    Added Hibernation feature

--- hypr/.config/hypr/scripts/powermenu.sh
+++ hypr/.config/hypr/scripts/powermenu.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-MENU="Shutdown|Suspend|Restart|Logout|Lock"
+MENU="Hibernate|Shutdown|Suspend|Restart|Logout|Lock"
 DIALOG_RESULT=$(echo $MENU | rofi -sep "|" -dmenu -i -p "System Configuration" -hide-scrollbar -tokenize -lines 6 -width 50 -padding 50 -disable-history)
 
 echo "This result is : $DIALOG_RESULT"
 sleep 1;
 
-if [ "$DIALOG_RESULT" = "Shutdown" ];
+if [ "$DIALOG_RESULT" = "Hibernate" ];
+then
+	exec systemctl hibernate
+
+elif [ "$DIALOG_RESULT" = "Shutdown" ];
 then
 	exec systemctl poweroff
 
