49f36cde87ee6f6c4f63f59653b6520fc060efe2
RaiderDude100 <Raider_Dude100@hotmail.com>
2026-02-02 19:14:31 +0300

    Some more changes

--- Scripts/Post/post.sh
+++ Scripts/Post/post.sh
@@ -18,48 +18,10 @@ gsettings set org.gnome.desktop.wm.preferences button-layout ''
 
 waypaper --random
 
-# --- Oh My Zsh unattended install ---
-export RUNZSH=no
-export CHSH=no
-export KEEP_ZSHRC=yes
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# --- Set zsh as default shell ---
-ZSH_PATH="$(command -v zsh)"
-
-
-# --- Oh My Zsh custom paths ---
-ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME_DIR/.oh-my-zsh/custom}"
-
-# --- Install Powerlevel10k theme ---
-if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
-  git clone --depth=1 \
-    https://github.com/romkatv/powerlevel10k.git \
-    "$ZSH_CUSTOM/themes/powerlevel10k"
-fi
-
-# --- Install fast-syntax-highlighting plugin ---
-if [ ! -d "$ZSH_CUSTOM/plugins/fast-syntax-highlighting" ]; then
-  git clone --depth=1 \
-    https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
-    "$ZSH_CUSTOM/plugins/fast-syntax-highlighting"
-fi
-
-# --- Cloudflare Warp ---
-sudo systemctl enable --now warp-svc
-sudo systemctl start warp-svc
-warp-cli registration new
-warp-cli mode warp+doh
-warp-cli connect
-warp-cli status
-
 # --- Firewall ---
 sudo systemctl enable --now ufw
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
-sudo ufw allow in on CloudflareWARP
-sudo ufw allow out on CloudflareWARP
 sudo ufw enable
 
 yay -Syu
@@ -68,6 +30,8 @@ tldr --update
 
 cd "$HOME/dotfiles"
 touch installed
+
+sudo chsh $USER_NAME -s /usr/bin/fish
 
 echo "Post installation scripts finished. Rebooting..."
 
