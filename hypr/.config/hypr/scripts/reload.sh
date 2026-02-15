f039a445aeffb357af9c5cc1a42833116646b118
RaiderDude100 <raider_dude100@hotmail.com>
2026-02-15 13:35:57 +0300

    More wallpapers

--- hypr/.config/hypr/scripts/reload.sh
+++ hypr/.config/hypr/scripts/reload.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+hyprctl reload # Reload hyprland configuration
+
 pkill waybar # Kill waybar
 pkill swaync # Kill swaync
+
 waybar & # Start waybar
 waybar -c ~/.config/waybar/osd.jsonc -s ~/.config/waybar/osd.css & # Start osd
 swaync & # Start swaync
