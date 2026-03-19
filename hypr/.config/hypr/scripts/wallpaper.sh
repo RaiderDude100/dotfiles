0f8c2e988d3f86001827aded53376c6d6f63f775
RaiderDude100 <raider_dude100@hotmail.com>
2026-03-19 10:31:16 +0300

    Fixed wallpaper script bug

--- hypr/.config/hypr/scripts/wallpaper.sh
+++ hypr/.config/hypr/scripts/wallpaper.sh
@@ -15,8 +15,8 @@ wallust run $wallpaper
 # Use imagemagick to generate different versions of the current wallpaper
 width=$(hyprctl monitors -j | jq 'map(select(.focused)) | .[0].width')
 scaled_width=$(echo "$width * 0.5" | bc)
-height=$(hyprctl monitors -j | jq 'map(select(.focused)) | .[0].width')
-scaled_height=$(echo "$width * 1" | bc)
+height=$(hyprctl monitors -j | jq 'map(select(.focused)) | .[0].height')
+scaled_height=$(echo "$height * 0.5" | bc)
 magick $wallpaper -resize 512x512^ -gravity center -extent 512x512 ~/.cache/profile_squared.jpg
 magick $wallpaper -gravity center -extent $scaled_widthx$scaled_height^ ~/.cache/profile_rectangle.jpg
 magick $wallpaper ~/.cache/wallpaper.jpg
