205540f877d046d9f4324c7c977870f545e6d4fd
RaiderDude100 <raider_dude100@hotmail.com>
2026-05-01 21:47:30 +0530

    More fixes

--- hypr/.config/hypr/scripts/search.sh
+++ hypr/.config/hypr/scripts/search.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run fzf with bat preview
-FILE=$(fzf --algo=v2 -e +i --style=full --color=dark,pointer:red,border:blue,prompt:red --wrap --preview 'bat --style=header-filename,header-filesize,numbers -f --theme=auto --line-range :500 {}')
+FILE=$(fzf --algo=v2 +x -e +i --style=full --color=dark,pointer:red,border:blue,prompt:red --wrap --preview 'bat --style=header-filename,header-filesize,numbers -f --theme=auto --line-range :500 {}')
 
 # If a file was selected, open it in nvim
 if [[ -n "$FILE" ]]; then
