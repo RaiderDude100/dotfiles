43af2c10eab662ebb76d16013c59eaa293ea49c1
RaiderDude100 <raider_dude100@hotmail.com>
2026-05-11 18:59:36 +0530

    Fixed some issues

--- Scripts/Install/install-programs.sh
+++ Scripts/Install/install-programs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yay -S cmatrix-git evince fastfetch file-roller fzf gapless gnome-calculator gnome-disk-utility gnome-text-editor hyprland hyprcursor hyprgraphics hypridle hyprlock hyprland-qt-support hyprland-guiutils hyprshot hyprutils hyprlang wlsunset kitty libreoffice-fresh matugen python-pywalfox mirage mpv nwg-look nwg-displays qt5ct qt6ct rofi steam mako swww thunar nemo vim wallust waybar waypaper zed opencode-bin nvim fuzzel alacritty --needed --noconfirm
+yay -S cmatrix-git evince fastfetch file-roller fzf gapless gnome-calculator gnome-disk-utility gnome-text-editor hyprland hyprcursor hyprgraphics hypridle hyprlock hyprland-qt-support hyprland-guiutils hyprshot hyprutils hyprlang wlsunset kitty libreoffice-fresh matugen python-pywalfox mirage mpv nwg-look nwg-displays qt5ct qt6ct rofi steam mako swww thunar nemo vim wallust waybar waypaper zed opencode-bin nvim fuzzel alacritty helium-browser-bin --needed --noconfirm
 
 echo "Installed programs"
 exit 0 
