bf6ccb8e05f523cffaf3a6040883c1af1e6653ac
RaiderDude100 <raider_dude100@hotmail.com>
2026-05-15 21:50:45 +0530

    Some updates and theme changes

--- Scripts/Post/post.sh
+++ Scripts/Post/post.sh
@@ -32,7 +32,18 @@ tldr --update
 cd "$HOME/dotfiles"
 touch installed
 
-sudo chsh $USER_NAME -s /usr/bin/fish
+echo "Choose shell"
+echo "1 for zsh"
+echo "2 for fish"
+read -rp "Enter your choice: " choice
+
+if [ $choice == "1" ] ; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+elif [ $choice == "2" ] ; then
+    sudo chsh $USER_NAME -s /usr/bin/fish
+fi
 
 echo "Post installation scripts finished. Rebooting..."
 
